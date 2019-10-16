1e9457b9367d09e82f62426a519db1fc0a32ec01
kevteg <kevteg05@gmail.com>
2019-10-16 17:50:32 -0400

    rename of field

--- helloworks/hwclient.py
+++ helloworks/hwclient.py
@@ -83,30 +83,30 @@ class HwClient(object):
         response = self.request.get(get_workflow_url, token)
         return response.json().get('data')
 
-    def get_workflow_instance_document(self, workflow_instance_id, document_id, get_stream=False):
+    def get_workflow_instance_document(self, workflow_instance_id, document_id, get_request=True):
         """
             Method to get a given document of a workflow instance
-            If get_stream is set to true it will return the request instead of writing the file
+            If get_request is set to true it will return the request instead of writing the file
         """
         get_workflow_document_url = self.endpoints.get_workflow_instance_document(workflow_instance_id, document_id)
-        return self._file_endpoints(get_workflow_document_url, f'{document_id}.pdf', get_stream)
+        return self._file_endpoints(get_workflow_document_url, f'{document_id}.pdf', get_request)
 
 
-    def get_workflow_instance_audit_trail(self, workflow_instance_id, get_stream=False):
+    def get_workflow_instance_audit_trail(self, workflow_instance_id, get_request=True):
         """
             Method to get the audit trail of a workflow instance
-            If get_stream is set to true it will return the request instead of writing the file
+            If get_request is set to true it will return the request instead of writing the file
         """
         get_workflow_audit_trail_url = self.endpoints.get_workflow_instance_audit_trail(workflow_instance_id)
-        return self._file_endpoints(get_workflow_audit_trail_url, f'{workflow_instance_id}-audit-trail.pdf', get_stream)
+        return self._file_endpoints(get_workflow_audit_trail_url, f'{workflow_instance_id}-audit-trail.pdf', get_request)
 
-    def get_workflow_instance_documents(self, workflow_instance_id, get_stream=False):
+    def get_workflow_instance_documents(self, workflow_instance_id, get_request=True):
         """
             Method to get the documents of a workflow instance
-            If get_stream is set to true it will return the request instead of writing the file
+            If get_request is set to true it will return the request instead of writing the file
         """
         get_workflow_documents_url = self.endpoints.get_workflow_instance_documents(workflow_instance_id)
-        return self._file_endpoints(get_workflow_documents_url, f'{workflow_instance_id}-files.zip', get_stream)
+        return self._file_endpoints(get_workflow_documents_url, f'{workflow_instance_id}-files.zip', get_request)
 
     def get_workflow_instance_steps(self, workflow_instance_id):
 
@@ -132,10 +132,10 @@ class HwClient(object):
     def save_setting_with_logo_file(self, **kwars):
         raise NotImplementedError('This method has not been implemented yet')
 
-    def _file_endpoints(self, endpoint, file_name, get_stream):
+    def _file_endpoints(self, endpoint, file_name, get_request):
         token = self.access.get_jwt_token()
         request = self.request.get(endpoint, token)
-        if get_stream:
+        if get_request:
             return request
         return _file_downloader(file_name, request)
 
