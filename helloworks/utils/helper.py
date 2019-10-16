28141a888554e784e1c557137e5d85f6ea2857cb
kevteg <kevteg05@gmail.com>
2019-10-16 17:50:48 -0400

    logger in case of error

--- helloworks/utils/helper.py
+++ helloworks/utils/helper.py
@@ -1,11 +1,19 @@
+import logging
+logger = logging.getLogger(__name__)
+
 
 def _file_downloader(file_name, file):
     with open(file_name, "wb") as f:
-        for chunk in file.iter_content(chunk_size=8192): 
-            if chunk:
-                f.write(chunk)
-        else:
-            return True
+        try:
+            for chunk in file.iter_content(chunk_size=8192): 
+                if chunk:
+                    f.write(chunk)
+            else:
+                return True
+        except:
+            logger.error('Could not write file')
+        return False
+
 
 def _clean_dict(dictionary):
     clean_dict = {**dictionary}
