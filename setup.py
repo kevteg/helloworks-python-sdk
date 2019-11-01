f6e1a86a4a136d5357fa24f327890b2afa14e946
Kevin Hernandez <kevteg05@gmail.com>
2019-11-01 18:20:06 -0400

    markdown type

--- setup.py
+++ setup.py
@@ -11,6 +11,7 @@ setup(
     version='0.2',
     description="A Python wrapper for the HelloWorks API  (https://portal.helloworks.com/)",
     long_description=readme(),
+    long_description_content_type="text/markdown",
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3.4",
