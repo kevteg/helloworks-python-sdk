8fb87722f62cda60b290d1f48b2294fc9b7bb4a5
Kevin Hernandez <kevteg05@gmail.com>
2019-11-01 18:11:40 -0400

    0.2 version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def readme():
 
 setup(
     name='helloworks-python-sdk',
-    version='0.1',
+    version='0.2',
     description="A Python wrapper for the HelloWorks API  (https://portal.helloworks.com/)",
     long_description=readme(),
     classifiers=[
