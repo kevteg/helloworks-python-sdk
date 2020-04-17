bdd8454a74818b0fcf16d27c58811bc112533b5a
Kevin Hernandez <kevteg05@gmail.com>
2020-04-17 12:34:38 -0500

    Adding new method

--- helloworks/utils/__init__.py
+++ helloworks/utils/__init__.py
@@ -1,5 +1,5 @@
 from .api_endpoints import ApiEndpoints
 from .request import HwRequest
 from .access import HwAccess
-from .helper import _file_downloader, _clean_dict
+from .helper import _file_downloader, _clean_dict, _file_extension
 __all__ = [ApiEndpoints, HwRequest, HwAccess]
